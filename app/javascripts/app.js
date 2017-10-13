d0c3d5843ca85548af4adf3a75f51821736a0afd
Etienne FICHOT <fichotetienne@gmail.com>
2017-10-13 14:39:56 +0200

    ProofOfExistence contract

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -43,7 +43,7 @@ window.App = {
                 console.log(val);
                 console.log(candidates[name]);
                 $('#' + candidates[name]).html('val');
-            })
+            });
         });
     },
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Voting = artifacts.require("./Voting.sol");
+var ProofOfExistence = artifacts.require("./ProofOfExistence.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Voting);
+  deployer.deploy(ProofOfExistence);
 };
